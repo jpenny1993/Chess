af3c458d1e83bda86e09c0c9d02e2e51ed31e9db
John Penny <jpenny1993@users.noreply.github.com>
2022-02-19 21:19:50 +0000

    Write paths for the Queen

--- Chess/Pieces/Queen.cs
+++ Chess/Pieces/Queen.cs
@@ -9,4 +9,58 @@ public sealed class Queen : Piece
     {
         Position = new (x, y);
     }
+    
+    
+    public override IEnumerable<IEnumerable<Position>> TheoreticalPaths()
+    {
+        var leftDown = Enumerable
+            .Range(0, Math.Min(Position.X - Position.MinX, Position.Y - Position.MinY))
+            .Where(offset => Position.IsValid(Position.X - offset, Position.Y - offset))
+            .Select(offset => new Position(Position.X - offset, Position.Y - offset))
+            .ToArray();
+        
+        var leftUp = Enumerable
+            .Range(0, Math.Min(Position.X - Position.MinX, Position.MaxY - Position.Y))
+            .Where(offset => Position.IsValid(Position.X - offset, Position.Y + offset))
+            .Select(offset => new Position(Position.X - offset, Position.Y + offset))
+            .ToArray();
+        
+        var rightDown = Enumerable    
+            .Range(0, Math.Min(Position.MaxX - Position.X, Position.Y - Position.MinY))
+            .Where(offset => Position.IsValid(Position.X + offset, Position.Y - offset))
+            .Select(offset => new Position(Position.X - offset, Position.Y - offset))
+            .ToArray();
+        
+        var rightUp = Enumerable
+            .Range(0, Math.Min(Position.MaxX - Position.X, Position.MaxY - Position.Y))
+            .Where(offset => Position.IsValid(Position.X + offset, Position.Y + offset))
+            .Select(offset => new Position(Position.X + offset, Position.Y + offset))
+            .ToArray();
+
+        var left = Enumerable
+            .Range(0, Position.X - Position.MinX)
+            .Where(offset => Position.IsValidX(Position.X - offset))
+            .Select(offset => new Position(Position.X - offset, Position.Y))
+            .ToArray();
+        
+        var right = Enumerable
+            .Range(Position.X, Position.MaxX - Position.X)
+            .Where(Position.IsValidX)
+            .Select(x => new Position(x, Position.Y))
+            .ToArray();
+        
+        var down = Enumerable
+            .Range(0, Position.Y)
+            .Where(offset => Position.IsValidY(Position.Y - offset))
+            .Select(offset => new Position(Position.X, Position.Y - offset))
+            .ToArray();
+        
+        var up = Enumerable
+            .Range(Position.Y, Position.MaxY - Position.Y)
+            .Where(Position.IsValidY)
+            .Select(y => new Position(Position.X, y))
+            .ToArray();
+        
+        return new[] { leftDown, left, leftUp, up, rightUp, right, rightDown, down };
+    }
 }
